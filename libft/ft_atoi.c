5cca169b68c73904657a3a82a950525e703ae7ba
xchalle <xchalle@student.42.fr>
2021-10-22 16:58:45 +0200

    ok

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: xchalle <xchalle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 15:46:08 by xchalle           #+#    #+#             */
-/*   Updated: 2021/10/22 16:45:48 by xchalle          ###   ########.fr       */
+/*   Updated: 2021/10/22 16:57:51 by xchalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ long long	ft_atoi(const char *str, int i, long long d, int c)
 	{
 		d = d * 10 + (str[i] - 48);
 		i++;
+		if (d > 2147483648)
+			return (2147483648);
 	}
 	return (d * c);
 }
